c625a9e588dbe6a3ac3f5ef0aed6227fd3f5dee8
Đorđe Matić <djmatic@agfbl.org>
2024-12-13 15:26:23 +0100

    add header component

---
+++ src/lib/components/custom-ui/header/contact-link.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
+	import Typography from '$lib/components/custom-ui/typography';
+	import type { ContactInfo } from '$lib/components/custom-ui/header';
+
+	type ContactLinkProps = ContactInfo & { class?: HTMLAnchorAttributes['class'] };
+
+	let { text, href, icon: Icon, class: className }: ContactLinkProps = $props();
+</script>
+
+<a {href} target="_blank" class={className}>
+	<Icon class="size-5 shrink-0" />
+	<Typography tag="span" variant="textSmall">{text}</Typography>
+</a>
---
+++ src/lib/components/custom-ui/header/header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { ContactLink, contactInfo } from '$lib/components/custom-ui/header';
+	import Typography from '$lib/components/custom-ui/typography';
+</script>
+
+<header>
+	<Typography tag="h2" variant="heading" fontWeight="bold">Đorđe Matić</Typography>
+
+	<Typography class="mt-2">Software Developer | Frontend Focused</Typography>
+	<Typography>Banja Luka, Serb Republic, Bosnia and Herzegovina</Typography>
+
+	<div class="mt-4 grid gap-2 md:grid-cols-2">
+		{#each contactInfo as data (data.id)}
+			<ContactLink {...data} class="flex max-w-fit items-center gap-2" />
+		{/each}
+	</div>
+</header>
