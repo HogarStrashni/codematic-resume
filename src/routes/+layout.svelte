64144f8364749e52b4d4a492f7ebde1beac7c92b
Đorđe Matić <djmatic@agfbl.org>
2024-12-13 15:27:15 +0100

    implement header - first iteration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import Header from '$lib/components/custom-ui/header';
+
 	let { children } = $props();
 </script>
 
 <h1 class="sr-only">Djordje Matic's resume</h1>
 
-<div class="border" aria-label="Resume Content">
-	<header class="flex justify-center border">
-		<h2 class="text-3xl">Header</h2>
-	</header>
+<div aria-label="Resume Content">
+	<Header />
 	{@render children()}
 </div>
