c8bc60ec7a59e15bde783d3c4fb6f80aebb2f142
Đorđe Matić <djmatic@agfbl.org>
2024-12-13 14:42:31 +0100

    implement typography component - first iteration

---
+++ src/lib/components/custom-ui/typography/typography.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { cn } from '$lib/utils/tailwind';
+	import type { Snippet } from 'svelte';
+	import { textVariants, type TextVariantProps } from './text-variants';
+
+	type HTMLTextElement = 'h2' | 'h3' | 'p' | 'span';
+
+	type TypographyProps = Partial<{
+		children: Snippet;
+		tag: HTMLTextElement;
+		variant: TextVariantProps['variant'];
+		fontWeight: TextVariantProps['fontWeight'];
+		class: string;
+	}>;
+
+	let {
+		children,
+		tag = 'p',
+		variant = 'text',
+		fontWeight,
+		class: className
+	}: TypographyProps = $props();
+</script>
+
+<svelte:element this={tag} class={cn(textVariants({ variant, fontWeight, className }))}>
+	{@render children?.()}
+</svelte:element>
