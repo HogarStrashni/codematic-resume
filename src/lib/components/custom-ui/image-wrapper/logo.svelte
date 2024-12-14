e65518e699e7f6fb1c33b9c427e3842df89b7422
Đorđe Matić <djmatic@agfbl.org>
2024-12-14 18:59:08 +0100

    add image-wrapper / logo component

---
+++ src/lib/components/custom-ui/image-wrapper/logo.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	import { cn } from '$lib/utils/tailwind';
+
+	type LogoProps = {
+		src: string;
+		alt?: string;
+		class?: SvelteHTMLElements['img']['class'];
+	};
+	let { src, alt = '', class: className }: LogoProps = $props();
+</script>
+
+<div class={cn('size-8 shrink-0 overflow-hidden rounded border', className)}>
+	<img {src} {alt} width={32} height={32} />
+</div>
