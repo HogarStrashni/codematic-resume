72b6601c58b525a3086d37400f679afd499bb6ba
Đorđe Matić <djmatic@agfbl.org>
2024-12-14 21:08:03 +0100

    update deps (bug fix) and add projects data

---
+++ src/lib/data/projects.ts
@@ -0,0 +1,38 @@
+export const projectsData = [
+	{
+		title: 'Mobilnost Website',
+		description: {
+			heading: 'Next.js & Sanity Web Application - In Development',
+			text: 'The project focused on creating a seamless content management experience, where articles could be easily authored, updated, and displayed in real-time. This project enhanced my skills in both frontend and backend technologies and strengthened my ability to build scalable, content-driven web applications.'
+		},
+		link: 'https://mobilnost-production-test.vercel.app/',
+		github: 'https://github.com/hogarstrashni/mobilnost'
+	},
+	{
+		title: 'Sitec Website',
+		description: {
+			heading: 'Astro + React Web Application - In Development',
+			text: "The goal was to leverage Astro's unique capabilities for optimizing performance and content delivery while working with a range of technologies and integrations. This project strengthened my understanding of modern web development practices and enabled me to apply my skills in creating scalable, high-performance web applications."
+		},
+		link: 'https://sitec.dev/',
+		github: null
+	},
+	{
+		title: 'My Movie List',
+		description: {
+			heading: 'ReactJS Web Application (Early Learning Phase)',
+			text: 'This small project was created as part of my job interview preparation to demonstrate my ability to build dynamic, data-driven applications with React, while integrating a REST API. The goal was to develop a movie list app that enables users to search for movies and view detailed information by fetching data from an external API.'
+		},
+		link: 'https://mymovielist-demo.vercel.app/',
+		github: 'https://github.com/hogarstrashni/movies-demo-app'
+	},
+	{
+		title: 'Students App',
+		description: {
+			heading: 'ReactJS Web Application (Early Learning Phase)',
+			text: 'This small project represents the beginning of my journey with React and Express.js. The main objective was to familiarize myself with core web development concepts and build a strong foundation for creating modern web applications using React. Throughout the process, I focused on learning both fundamental and essential features of React, as well as implementing practical solutions with various tools and libraries.'
+		},
+		link: 'https://students-app-react.vercel.app/',
+		github: 'https://github.com/hogarstrashni/students-app-react'
+	}
+];
