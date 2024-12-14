c9820fdf2d29a68ab36492ba3e94c1b41a3c9469
Đorđe Matić <djmatic@agfbl.org>
2024-12-14 17:40:48 +0100

    implement data-formatter utils; experience-data improvements

--- src/lib/data/experience-data.ts
+++ src/lib/data/experience-data.ts
@@ -7,10 +7,9 @@ export const experienceData = [
 	{
 		title: 'Software developer',
 		company: 'Sitec LLC',
-		location: 'Banja Luka',
 		logo: SitecLogo,
-		dateStart: '2023-03-16',
-		dateEnd: null,
+		startDate: '2023-03-16',
+		endDate: null,
 		description:
 			'Developed web applications using modern frameworks, and integrating backend services with tools like Supabase and Turso. Learned to optimize web applications for performance and user experience by leveraging TypeScript and various UI libraries. Gained expertise in managing database interactions and improving application architecture with advanced tools and frameworks. Collaborated in building full-stack solutions, focusing on efficient and scalable designs.',
 		technologies: [
@@ -22,20 +21,20 @@ export const experienceData = [
 			'Typescript',
 			'Supabase',
 			'Turso',
-			'Drizzle ORM'
+			'Drizzle ORM',
+			'Sanity'
 		]
 	},
 	{
 		title: 'Junior React Developer',
 		company: 'ElevateBits',
-		location: 'Banja Luka',
 		logo: ElevateBitsLogo,
-		dateStart: '2022-12-01',
-		dateEnd: '2023-03-16',
+		startDate: '2022-12-01',
+		endDate: '2023-03-16',
 		description:
 			'Learned to develop and maintain web applications using React JS, focusing on building responsive user interfaces. Gained experience in integrating APIs and handling data efficiently through various tools. Enhanced skills in managing application state and forms, as well as working with custom formatting for numbers and dates. Gained understanding of organizing application file structures and ensuring smooth navigation and user experience. Collaborated on version control and improved project workflows within the team.',
 		technologies: [
-			'Frontend development using React JS / Vite / TypeScript',
+			'React JS / TypeScript',
 			'React Router / Deep Linking',
 			'REST API',
 			'Axios / React Query',
@@ -43,28 +42,26 @@ export const experienceData = [
 			'React Hook Forms / Zod',
 			'Tailwind CSS / Tailwind UI / Headless UI',
 			'Responsive UI Design',
-			'GIT',
-			'Custom number and date formatting',
-			'Application File Structure - basic principles'
+			'GIT'
 		]
 	},
 	{
 		title: 'Web Development Intern',
 		company: 'Sitec LLC',
-		location: 'Banja Luka',
 		logo: SitecLogo,
-		dateStart: '2022-02-01',
-		dateEnd: '2022-12-16',
+		startDate: '2022-02-01',
+		endDate: '2022-12-16',
 		description:
 			'Gained experience in developing user interfaces and ensuring their responsiveness across different devices. Learned to integrate backend services and enhance web application functionality and developed skills in designing and building web solutions, with a focus on performance and user experience.',
 		technologies: [
-			'HTML, CSS',
+			'HTML',
+			'CSS',
 			'JavaScript',
-			'Frontend development using React JS',
+			'React JS',
 			'Responsive UI Design',
 			'REST API',
-			'Express JS / MERN stack basic principles',
-			'JWT / Authentication and Authorization workflows - basic principles',
+			'Express JS',
+			'JWT / Authentication and Authorization workflows',
 			'GIT',
 			'Tailwind CSS',
 			'TypeScript'
@@ -73,14 +70,14 @@ export const experienceData = [
 	{
 		title: 'Civil Engineer',
 		company: 'Geoput d.o.o.',
-		location: 'Banja Luka',
 		logo: GeoputLogo,
-		dateStart: '2022-01-31',
-		dateEnd: '2019-01-15',
+		startDate: '2019-01-15',
+		endDate: '2022-01-31',
 		description:
 			'Preparation of project and spatial planning documentation in the field of hydrotechnics. Lead designer for technical solutions in water management, infrastructure projects, and supervision.',
 		technologies: [
-			'AutoCAD and AutoCAD Civil 3D',
+			'AutoCAD',
+			'AutoCAD Civil 3D',
 			'MS Office',
 			'AutoCAD LISP programming',
 			'HEC-RAS',
@@ -90,12 +87,13 @@ export const experienceData = [
 	{
 		title: 'Civil Engineer',
 		company: 'Routing d.o.o.',
-		location: 'Banja Luka',
 		logo: RoutingLogo,
-		dateStart: '2012-03-20',
-		dateEnd: '2018-09-30',
+		startDate: '2012-03-20',
+		endDate: '2018-09-30',
 		description:
 			'Focusing on the preparation of project and spatial planning documentation in the field of hydrotechnics. Involved in designing technical solutions for water management and infrastructure projects.',
 		technologies: ['AutoCAD', 'MS Office', 'EPANET']
 	}
 ];
+
+export type ExperienceData = (typeof experienceData)[number];
