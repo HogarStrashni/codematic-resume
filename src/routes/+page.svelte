20eef7cd911f94ecaeff0e1eca7a474d62c2fdb2
Đorđe Matić <djmatic@agfbl.org>
2024-12-14 11:23:12 +0100

    add placeholders to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,31 @@
 <script lang="ts">
-	import Typography from '$lib/components/custom-ui/typography';
 	import Header from '$lib/components/custom-ui/header';
+	import SectionWrapper from '$lib/components/custom-ui/section';
+	import Summary from '$lib/components/custom-ui/section/summary';
 </script>
 
 <Header />
-<section class="mt-12">
-	<Typography tag="h3" variant="headingSmall" fontWeight="bold">Summary</Typography>
-	<Typography>
-		With a decade of experience as a civil engineer, I made the exciting transition to software
-		development, discovering a true passion for coding along the way. As a software developer, I
-		leverage my engineering background to devise effective solutions and enhance user experiences
-		through technology. My experience includes substantial work in JavaScript development, along
-		with technologies such as HTML, CSS, and frameworks like SvelteKit, Next.js, and Astro.
-		Committed to staying current with industry trends and continuously expanding my skill set, I am
-		eager to tackle new challenges and explore full-stack development by integrating my front-end
-		knowledge with back-end technologies.
-	</Typography>
-</section>
+
+<SectionWrapper title="Summary" class="mt-12 space-y-4">
+	<Summary class="space-y-2" />
+</SectionWrapper>
+
+<SectionWrapper title="Work Experience" class="mt-12 space-y-4">
+	<div>Experience placeholder</div>
+</SectionWrapper>
+
+<SectionWrapper title="Education" class="mt-12 space-y-4">
+	<div>Education placeholder</div>
+</SectionWrapper>
+
+<SectionWrapper title="Skills" class="mt-12 space-y-4">
+	<div>Skills placeholder</div>
+</SectionWrapper>
+
+<SectionWrapper title="Projects" class="mt-12 space-y-4">
+	<div>Projects placeholder</div>
+</SectionWrapper>
+
+<SectionWrapper title="Hobby and personal qualities" class="mt-12 space-y-4">
+	<div>Hobby and personal qualities placeholder</div>
+</SectionWrapper>
