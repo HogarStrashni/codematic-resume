bbbe4963d15c4547eb0781801b76e2cbee5264db
Đorđe Matić <djmatic@agfbl.org>
2024-12-15 14:42:27 +0100

    add shadow to action button / links

--- src/lib/components/custom-ui/section/projects/actions.svelte
+++ src/lib/components/custom-ui/section/projects/actions.svelte
@@ -10,7 +10,7 @@
 		href={link}
 		aria-label="Link to Web App"
 		target="_blank"
-		class="inline-block shrink-0 rounded border px-5 py-2 duration-200 hover:bg-gray-50"
+		class="inline-block shrink-0 rounded border px-5 py-2 shadow duration-200 hover:bg-gray-50 active:shadow-none"
 	>
 		<Link class="size-4" />
 	</a>
@@ -20,7 +20,7 @@
 			href={github ?? undefined}
 			aria-label="Link to Github"
 			target="_blank"
-			class="inline-block shrink-0 rounded border px-5 py-2 duration-200 hover:bg-gray-50"
+			class="inline-block shrink-0 rounded border px-5 py-2 shadow duration-200 hover:bg-gray-50 active:shadow-none"
 		>
 			<Github class="size-4" />
 		</a>
