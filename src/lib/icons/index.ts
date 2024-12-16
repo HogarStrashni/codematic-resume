13184aeade20be9c9baff356a76ea127c3b7463f
Đorđe Matić <djmatic@agfbl.org>
2024-12-16 08:37:26 +0100

    add icons download and printer

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -5,7 +5,9 @@ import Phone from './phone.svelte';
 import Linkedin from './linkedin.svelte';
 import Github from './github.svelte';
 import Link from './link.svelte';
+import Download from './download.svelte';
+import Printer from './printer.svelte';
 
 type IconProps = { class?: SvelteHTMLElements['svg']['class'] };
 
-export { MailOpen, Phone, Linkedin, Github, Link, type IconProps };
+export { MailOpen, Phone, Linkedin, Github, Link, Download, Printer, type IconProps };
