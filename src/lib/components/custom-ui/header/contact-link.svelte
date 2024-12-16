dca0d6726e38a159be794b9606081b3b213b96e5
Đorđe Matić <djmatic@agfbl.org>
2024-12-16 08:40:26 +0100

    fix bug - add tailwind group name

--- src/lib/components/custom-ui/header/contact-link.svelte
+++ src/lib/components/custom-ui/header/contact-link.svelte
@@ -10,12 +10,12 @@
 	let { text, href, icon: Icon, class: className }: ContactLinkProps = $props();
 </script>
 
-<a {href} target="_blank" class={cn('group', className)}>
+<a {href} target="_blank" class={cn('group/link', className)}>
 	<Icon class="size-5 shrink-0" />
 	<Typography
 		tag="span"
 		variant="textSmall"
-		class="underline underline-offset-4 duration-200 group-active:text-purple-600 lg:group-hover:text-blue-600 print:text-blue-600"
+		class="underline underline-offset-4 duration-200 group-active:text-purple-600 lg:group-hover/link:text-blue-600 print:text-blue-600"
 	>
 		{text}
 	</Typography>
