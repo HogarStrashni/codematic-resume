83fe4abad570a8e0c3ce0e7fe22a5f18752087ed
Đorđe Matić <djmatic@agfbl.org>
2024-12-18 10:13:00 +0100

    modify OG meta tags content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,10 +7,13 @@
 
 <svelte:head>
 	<title>Đorđe Matić - Resume</title>
-	<meta name="description" content="Codematic Đorđe Matić Resume - Personal Website" />
+	<meta name="description" content="Codematic Đorđe Matić Resume - Website" />
 
-	<meta property="og:title" content="Personal Resume" />
-	<meta property="og:description" content="Codematic - Đorđe Matić Resume" />
+	<meta property="og:title" content="Codematic - Đorđe Matić - Resume" />
+	<meta
+		property="og:description"
+		content="Explore the professional background, skills, and accomplishments of Đorđe Matić, a passionate software developer."
+	/>
 	<meta property="og:image" content={`${$page.url.origin}/codematic-logo.png`} />
 	<meta property="og:url" content={`${$page.url.origin}`} />
 	<meta property="og:type" content="website" />
