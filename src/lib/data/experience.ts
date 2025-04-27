c488a081fc0d679a2016e710a392acf35e3036ac
Đorđe Matić <djmatic@agfbl.org>
2025-04-27 14:39:33 +0200

    update job htec; and update skills section

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -1,3 +1,4 @@
+import HtecLogo from '$lib/assets/htecgroup-logo.jpg?enhanced';
 import SitecLogo from '$lib/assets/sitec-llc-logo.jpg?enhanced';
 import ElevateBitsLogo from '$lib/assets/elevatebits-logo.jpg?enhanced';
 import GeoputLogo from '$lib/assets/geoput-logo.png?enhanced';
@@ -6,12 +7,29 @@ import RoutingLogo from '$lib/assets/routing-logo.png?enhanced';
 export const experienceData = [
 	{
 		title: 'Software developer',
+		company: 'HTEC',
+		logo: HtecLogo,
+		startDate: '2025-02-25',
+		endDate: null,
+		description:
+			'Integrating frontend applications with backend APIs while continuously enhancing user experience through performance optimization, accessibility best practices, and responsive design. Working closely with designers and backend developers to deliver seamless, user-focused interfaces. Actively contributing to an agile development process through code reviews, feature planning, and ongoing refinement of frontend workflows. Ensuring application functionality and user experience through comprehensive end-to-end (E2E) testing.',
+		technologies: [
+			'SvelteKit / Svelte',
+			'SvelteKit Superforms / Zod',
+			'TanStack Svelte Virtual',
+			'Typescript',
+			'REST API',
+			'Playwright'
+		]
+	},
+	{
+		title: 'Software developer',
 		company: 'Sitec LLC',
 		logo: SitecLogo,
 		startDate: '2023-03-16',
 		endDate: null,
 		description:
-			'Developed web applications using modern frameworks, and integrating backend services with tools like Supabase and Turso. Learned to optimize web applications for performance and user experience by leveraging TypeScript and various UI libraries. Gained expertise in managing database interactions and improving application architecture with advanced tools and frameworks. Collaborated in building full-stack solutions, focusing on efficient and scalable designs.',
+			'Develop web applications using modern frameworks, integrating backend services with tools like Supabase and Turso. Optimize performance and user experience by leveraging TypeScript and various UI libraries. Manage database interactions and enhance application architecture using advanced tools and frameworks. Collaborate on full-stack solutions with a focus on efficient, scalable design. Continuously stay updated with the latest industry trends to apply cutting-edge techniques and tools in development.',
 		technologies: [
 			'SvelteKit / Svelte',
 			'SvelteKit Superforms / Zod',
