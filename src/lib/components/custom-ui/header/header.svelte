2e48f922cabb588c59558da988713ad5bd210a29
Đorđe Matić <djordje.matic@inzenjer.org>
2025-05-19 20:34:21 +0200

    add header ui improvements

--- src/lib/components/custom-ui/header/header.svelte
+++ src/lib/components/custom-ui/header/header.svelte
@@ -11,13 +11,13 @@
 
 <header
 	class={cn(
-		'sticky top-16 -mx-1 flex -translate-y-16 items-center justify-end gap-2 bg-white/85 py-3 shadow-glass backdrop-blur-sm sm:-mb-4 print:hidden',
+		'sticky top-16 flex -translate-y-16 items-center justify-end gap-2 bg-white/85 py-3 shadow-glass backdrop-blur-sm sm:-mb-4 print:hidden',
 		className
 	)}
 >
 	<button
 		onclick={() => window.print()}
-		class="hidden flex-col items-center rounded border px-6 py-2 shadow-sm duration-200 active:shadow-none lg:flex lg:hover:bg-gray-50"
+		class="hidden flex-col items-center rounded border bg-white px-6 py-2 shadow-sm duration-200 active:shadow-none lg:flex lg:hover:bg-gray-50"
 	>
 		<Printer class="size-4" />
 		<Typography tag="span" variant="caption" fontWeight="light" class="italic">print</Typography>
@@ -25,7 +25,7 @@
 	<a
 		href="/djordje-matic-resume.pdf"
 		download="Djordje Matic - Resume.pdf"
-		class="flex items-center gap-1 rounded border px-4 py-2 shadow-sm duration-200 active:shadow-none lg:flex-col lg:gap-0 lg:hover:bg-gray-50"
+		class="flex items-center gap-1 rounded border bg-white px-4 py-2 shadow-sm duration-200 active:shadow-none lg:flex-col lg:gap-0 lg:hover:bg-gray-50"
 	>
 		<Download class="size-4" />
 		<Typography tag="span" variant="caption" fontWeight="light" class="italic">download</Typography>
